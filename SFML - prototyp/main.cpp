b60981f3cfe3180f6c2aba4f852da4063cce2718
Robert Kamiński <robix583@gmail.com>
2018-04-09 20:32:39 +0200

    Gotowe dla Kacpra

--- SFML - prototyp/main.cpp
+++ SFML - prototyp/main.cpp
@@ -5,6 +5,7 @@
 #include <SFML/Window.hpp> // dla okna SFML
 #include <cmath> // do przeksztalcen
 
+
 //w dependendiences -d zeby wlaczyc tryb dla debugoania
 
 // Wylaczenie konsoli |-|  Linker-> System -> subSystem -> Windows (/SUBSYSTEM:WINDOWS)
@@ -41,6 +42,8 @@ int main()
 	//sf::RenderWindow okno(sf::VideoMode(1280, 720), "Kelajno");
 	sf::Clock stoper;
 	//testowe(stoper);
+	int number_of_chunks = 1024;
+	int map_width = 32, map_height = 32;
 
 	//sf::Window oknoAplikacji(sf::VideoMode(1280, 720, 32), "Kelajno");// szer, wys, glebia bitowa
 	// mozna to podac bez argumentow - zwykla inicjalizacja ale wtedy tworzysz oknoAplikacji.create(...);
@@ -48,27 +51,44 @@ int main()
 	sf::RenderWindow oknoAplikacji(sf::VideoMode(1920, 1080, 32), "Kelajno", sf::Style::Fullscreen);//to opcja fullscreen
 
 	sf::Texture tekstura;//sluzy do wczytywania tekstury bo jest texture i image
-	tekstura.loadFromFile("C:/Users/Qaczka/Desktop/Moje/Grunt.png");//zwraca true lub false
+	tekstura.loadFromFile("Textures/Grunt.png");//zwraca true lub false
 	sf::Texture tekstura2;
-	tekstura2.loadFromFile("C:/Users/Qaczka/Desktop/Moje/Grunt2.png");
+	tekstura2.loadFromFile("Textures/Grunt2.png");
+	sf::Texture tekstura3;
+	tekstura3.loadFromFile("Textures/Drzewko.png");
 
-	sf::Sprite obrazek[4];
-	obrazek[0].setTexture(tekstura2);
-	obrazek[1].setTexture(tekstura);
-	obrazek[2].setTexture(tekstura2);
-	obrazek[3].setTexture(tekstura);
-	sf::FloatRect chunk_size = obrazek[1].getGlobalBounds();
+	//sf::Sprite obrazek[4];
+	sf::Sprite *obrazek = new sf::Sprite[number_of_chunks];
+	for (int i = 0; i < number_of_chunks; i++)
+	{
+		if (i % 2 == 0)
+		{
+			obrazek[i].setTexture(tekstura);
+		}
+		else
+		{
+			obrazek[i].setTexture(tekstura2);
+		}
+	}
+
+	sf::Sprite drzewo;
+	drzewo.setTexture(tekstura3);
+
+	sf::FloatRect chunk_size = obrazek[0].getGlobalBounds();//biore info odnosnie wymiarow chunka na przykladzie pierwszego
 
 
-	//obrazek[1].setRotation(45.f);//obrot o 45 stopni
-	//obrazek[2].setRotation(45.f);
+	for (int i = 0; i < map_height; i++)
+	{
+		for (int g = 0; g < map_width; g++)
+		{
+			obrazek[g + (i*map_width)].setPosition((oknoAplikacji.getSize().x * 0.5+250)-(g*chunk_size.width), (oknoAplikacji.getSize().y * 0.5-250) + (i*chunk_size.height));
+			//podwojna petla "i" dla nr wiersza i "g" dla kolumny - do szerokosci dodaje wielokrotnosci kolumnowe i wierszowe dla wysokosci
+			//250 na korekte
+		}
+	}
 
-
+	drzewo.setPosition((oknoAplikacji.getSize().x * 0.5), (oknoAplikacji.getSize().y * 0.5));
 	//obrazek[1].setPosition(obrazek[1].getPosition().x, obrazek[1].getPosition().y * 2);
-	obrazek[1].setPosition(oknoAplikacji.getSize().x * 0.5, oknoAplikacji.getSize().y * 0.5);
-	obrazek[2].setPosition((oknoAplikacji.getSize().x * 0.5)+chunk_size.width, (oknoAplikacji.getSize().y*0.5)+chunk_size.height);
-	obrazek[0].setPosition((oknoAplikacji.getSize().x * 0.5), (oknoAplikacji.getSize().y*0.5) + chunk_size.height);
-	obrazek[3].setPosition((oknoAplikacji.getSize().x * 0.5) + chunk_size.width, (oknoAplikacji.getSize().y*0.5));
 
 	/*
 	void setPosition( float x, float y );
@@ -79,10 +99,12 @@ int main()
 	*/
 
 	sf::View v = oknoAplikacji.getDefaultView();//widok ma byc taki jak okno tak jakby ciagnie z niego dane
-	//v.setSize(v.getSize().x, v.getSize().y * 2);//tak jak przy teksturze
+	v.setSize(v.getSize().x, v.getSize().y * 2);//tak jak przy teksturze skalujemy 2 wieksza wysoksoc
 	//v.setCenter(v.getSize() *.5f);//center bo bierzesz rozmiar i na pol .5 - 0.5
 	v.setRotation(45);
-	v.setViewport(sf::FloatRect(0.f, 0.25f, 1.f, 0.5f));//kamera o polowe 90 stopni czyli 0.5f w dol plus przesuniecie o cwierc w dol
+	//v.zoom(2.0f);
+	//v.move(0.f, 0.f);
+	v.setViewport(sf::FloatRect(0.f, 0.f, 1.f, 1.f));//kamera o polowe 90 stopni czyli 0.5f w dol plus przesuniecie o cwierc w dol
 
 	while (oknoAplikacji.isOpen())
 	{
@@ -105,14 +127,17 @@ int main()
 		}
 		oknoAplikacji.clear(sf::Color(0, 0, 0));
 		oknoAplikacji.setView(v);//ustawia widok
-		oknoAplikacji.draw(obrazek[1]);//wyswietla obrazek
-		oknoAplikacji.draw(obrazek[2]);
-		oknoAplikacji.draw(obrazek[3]);
-		oknoAplikacji.draw(obrazek[0]);
-		//oknoAplikacji.setView(oknoAplikacji.getDefaultView());//nie wiem czemu
+
+		for (int i = 0; i < number_of_chunks; i++)
+		{
+			oknoAplikacji.draw(obrazek[i]);//wyswietla chunki
+		}
+		oknoAplikacji.setView(oknoAplikacji.getDefaultView());//zeby zrzutowac drzewo prosto
+		oknoAplikacji.draw(drzewo);
+
 		oknoAplikacji.display();
-
-
 	}
+
+	delete[] obrazek;
 	return 0;
 }
